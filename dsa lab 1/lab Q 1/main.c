fc18d6fa4853389beee6482ac6caf85df0fe5b29
Parsley0 <147031484+Parsley0@users.noreply.github.com>
2024-02-05 06:24:37 +0300

    Add files via upload

---
+++ dsa lab 1/lab Q 1/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int summation(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int maximum(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = summation(arr, n);
+    int max = maximum(arr, n);
+
+    printf("Sum of the integers: %d\n", sum);
+    printf("Maximum integer: %d\n", max);
+
+    return 0;
+}
+
